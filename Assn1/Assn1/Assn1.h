85c2fb834ba7c61f1f3cd329f59aea884fe3f47e
P17Seongbin <sungbin0515@postech.ac.kr>
2019-03-15 01:18:11 +0900

    Maincode를 여러 파일로 분할

---
+++ Assn1/Assn1/Assn1.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/freeglut.h> 
+#include "System.h"
+#include "Player.h"
+#include "Ball.h"
+//물리엔진과 무관한, 순수히 Assn1을 위한 헤더파일이나 함수 등을 선언하는 헤더입니다.
+#define MAX(x,y) (x > y ? x : y)
+#define MIN(x,y) (x > y ? y : x)
+#define WIN_HOR 100
+#define WIN_VER 100
+#define CAM_SIZE 20
+#define BALL_SIZE 20
+
+using namespace std;
+void ReShape(int w, int h);
+void setOrtho2D(GameManager* GM, KeyHandler* keyhandler);
+void StepCaller(int dt);
+void AsciiKeyPress(unsigned char key, int x, int y);
+void AsciiKeyRelease(unsigned char key, int x, int y);
+void SpecialKeyPress(int key, int x, int y);
+void SpecialKeyRelease(int key, int x, int y);
+void DrawCaller(); 
+void InitScene(void);
+void RegisterCallback();
+void RegisterObject(GameManager* GM, KeyHandler* keyhandler);
