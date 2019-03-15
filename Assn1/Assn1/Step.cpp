b6f599c1f6652dde2dee7f55101ae0e7447734f3
RisingStockPrices <thswndms98@naver.com>
2019-03-15 17:37:35 +0900

    게임종료화면 만듬. 편의를 위해 일단 점수 1점 얻으면 게임종료하게 함

--- Assn1/Assn1/Step.cpp
+++ Assn1/Assn1/Step.cpp
@@ -20,15 +20,44 @@ void drawBitmapText(std::string string, float x, float y, float z)
 }
 void GameManager::Draw()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	for (std::vector<std::pair<std::string, Object*>>::iterator it = ObjectList.begin(); it != ObjectList.end(); it++)
-	{
-		it->second->Draw();
-	}
-	glColor3f(0, 0, 0);
-	drawBitmapText(std::to_string(score_1),10,80,0); 
-	drawBitmapText(std::to_string(score_2), 88, 80, 0);
+	int max = 1;
+	int size = 30;
+	int winner = 1;
 
+	glClear(GL_COLOR_BUFFER_BIT);
+	if (score_1 == max || score_2 == max) //game over
+	{
+		ObjectList[2].second->setSpeed(0, 0);//stop ball
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		glColor3f(1.0, 0.5, 0.2);
+		glBegin(GL_POLYGON);//draw 'window'
+		glVertex2f(WIN_HOR / 2 - size, WIN_VER / 2 - size);
+		glVertex2f(WIN_HOR / 2 - size, WIN_VER / 2 + size);
+		glVertex2f(WIN_HOR / 2 + size, WIN_VER / 2 + size);
+		glVertex2f(WIN_HOR / 2 + size, WIN_VER / 2 - size);
+		glEnd();
+
+		glColor3f(0, 0, 0);//print message
+		if (score_1 == max)
+		{
+			drawBitmapText("You WIN!!", WIN_HOR / 2-6, WIN_VER / 2,0);
+		}
+		else
+		{
+			drawBitmapText("You LOSE!!", WIN_HOR / 2-6, WIN_VER / 2, 0);
+		}
+
+	}
+	else {
+		for (std::vector<std::pair<std::string, Object*>>::iterator it = ObjectList.begin(); it != ObjectList.end(); it++)
+		{
+			it->second->Draw();
+		}
+		glColor3f(0, 0, 0);
+		drawBitmapText(std::to_string(score_1), 10, 80, 0);
+		drawBitmapText(std::to_string(score_2), 88, 80, 0);
+	}
 	glutPostRedisplay();
 	glutSwapBuffers();
 }
@@ -37,7 +66,8 @@ void GameManager::Step(int dt)
 	//1. 오브젝트 충돌 판정
 	Object* ball = getObjectList()[2].second;;
 	std::pair<float,float> pos=ball->getPos();
-	
+	float newy = WIN_VER/2;
+
 	calCollide();
 	//2. 각 Object의 Step을 호출
 	for (std::vector<std::pair<std::string, Object*>>::iterator it = ObjectList.begin(); it != ObjectList.end(); it++)
@@ -47,6 +77,7 @@ void GameManager::Step(int dt)
 	
 	if (pos.second < ball->getRadius())
 	{
+
 		if (pos.first < WIN_HOR / 2)
 		{
 			score_2++;
@@ -58,8 +89,8 @@ void GameManager::Step(int dt)
 			ball->setSpeed(-SPEED, -SPEED);
 		}
 
-		ball->setPos(WIN_HOR/2, 3*WIN_VER/4);
-	
+		newy += rand() % (WIN_VER / 2-1);//random y-start-position
+		ball->setPos(WIN_HOR/2, newy);
 	}
 	
 	//3. 죽은 오브젝트에 대한 데이터를 말소
