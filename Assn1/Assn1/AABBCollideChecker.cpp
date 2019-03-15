e388854811302da2c305e26a8dcb6f78cf82d842
P17Seongbin <sungbin0515@postech.ac.kr>
2019-03-15 15:25:41 +0900

    pair opeartor 삭제

--- Assn1/Assn1/AABBCollideChecker.cpp
+++ Assn1/Assn1/AABBCollideChecker.cpp
@@ -36,13 +36,7 @@ bool CheckCollide_RR(AABB *a, AABB *b)
 	{
 		//printf("%d", (posB.first < posA.first + sizeA.first));
 		return (posB.first < posA.first + sizeA.second);
-	}
-	//bool colX = posA.first + sizeA.first >= posB.first && posB.first + sizeB.first >= posB.first;
-	//bool colY = posA.second + sizeA.second >= posB.second && posB.second + sizeB.second >= posB.second;
-	/*
-	printf("%d", colX &&colY);
-	return colX && colY;*/
-	
+	}	
 }
 //Circle-Rect
 bool CheckCollide_CR(AABB *a, AABB *b)
@@ -53,14 +47,6 @@ bool CheckCollide_CR(AABB *a, AABB *b)
 	std::pair<float, float> posA = A->getPos(), posB = B->getPos();
 	std::pair<float, float> sizeB = B->getSize();
 	float radius = A->getRadius();
-	/*std::pair<float, float> diff = posA - posB;
-	std::pair<float,float> clamped = 
-		std::make_pair(clamp(diff.first,(-0.5f)*sizeB.first,(0.5f)*sizeB.first),clamp(diff.second, (-0.5f)*sizeB.second, (0.5f)*sizeB.second));
-	std::pair<float, float> closest = posB + clamped;
-	diff = closest - posA;
-	//printf("%d",sqrt(pow(diff.first, 2) + pow(diff.second, 2)) < A->getRadius());
-	//return sqrt(pow(diff.first, 2) + pow(diff.second, 2)) < A->getRadius();
-	*/
 	return ((posA.first >= posB.first - radius) && (posA.first <= posB.first + radius+sizeB.second)
 		&& (posA.second <= sizeB.first+radius));
 }
@@ -75,21 +61,9 @@ bool CheckCollide_CC(AABB *a,AABB *b)
 	CircleAABB *A = (CircleAABB*)a;
 	CircleAABB *B = (CircleAABB*)b;
 	float dist = getDist(A->getPos(), B->getPos());
-	//printf("%d", dist <= (A->getRadius() + B->getRadius()));
 	return dist <= (A->getRadius() + B->getRadius());
 }
-//pair operator+
-std::pair<float, float>& operator+(std::pair<float, float> &a, std::pair<float, float> &b)
-{
-	std::pair<float, float> res = std::make_pair(a.first + b.first, a.second + b.second);
-	return res;
-}
-//pair operator-
-std::pair<float, float>& operator-(std::pair<float, float> &a, std::pair<float, float> &b)
-{
-	std::pair<float, float> res = std::make_pair(a.first - b.first, a.second - b.second);
-	return res;
-}
+
 float getDist(std::pair<float, float> p1, std::pair<float, float> p2)
 {
 	return sqrt(pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2));
